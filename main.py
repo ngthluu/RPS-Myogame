c4420a812d8946825b753528a79d2d71e1045f07
Nguyen Luu <luu.nguyen2101@hcmut.edu.vn>
2019-10-16 08:47:21 +0700

    Add pygame

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 import os
 import ctypes
+import pygame
 
-# Change path to lib
+# Game constants
+FPS = 60
+WORLD_SIZE = WIDTH, HEIGHT = 960, 600
+WORLD_COLOR = 255, 255, 255
+GAME_TITLE = "RPS - Myo Demo V1.0.0"
+
+# Game variables
 os.chdir(os.getcwd() + r"\lib")
+connector = ctypes.cdll.LoadLibrary(os.getcwd() + r"\MyoData.dll")
 
-# Get main dll file
-dll = ctypes.cdll.LoadLibrary(os.getcwd() + r"\MyoData.dll")
+# Game init
+pygame.display.set_caption(GAME_TITLE)
+game_display = pygame.display.set_mode(WORLD_SIZE)
 
 
-if dll.myo_init() :
-
+# Game loop
+if connector.init():
     try:
         while True:
-            dll.myo_run()
-            pointer_emg = dll.get_emg_data()
-            arr = (ctypes.c_int * 8).from_address(pointer_emg)
-            data_list = [arr[i] for i in range(8)]
+            # Get data
+            connector.run(FPS)
+            pointer_emg = connector.get_emg_data()
+            temp = (ctypes.c_int * 8).from_address(pointer_emg)
+            data_list = [temp[i] for i in range(8)]
             print(data_list)
-        dll.myo_exit()
+
+            # Rendering game
+            
 
     except KeyboardInterrupt as e:
         print("Exiting...")
-        dll.myo_exit()
-    
+        pygame.quit()
+        connector.close()
+else:
+    print("Unable to connect to the Myo Armband !")
